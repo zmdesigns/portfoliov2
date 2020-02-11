c7bfaa1faf08f159e98b46e8a3a48d84ca58ee44
zmdesigns <zackmdesigns@gmail.com>
2020-02-11 14:19:07 -0800

    Added form handling code

---
+++ submit-form.php
@@ -0,0 +1,85 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../mail-info.php';
+require '/home/dh_nni3e9/zackmdesigns.com/vendor/autoload.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //validate user input
+    $name = $phone = $email = $error = "";
+    //email
+    if (empty($_POST["contact_email"])) {
+        $error = "Email is required";
+    } else {
+        $email = validate_input($_POST["contact_email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error = "Invalid email format";
+        }
+    }
+    //phone
+    if (empty($_POST["contact_phone"])) {
+        $error = "Phone is required";
+    } else {
+        $phone = validate_input($_POST["contact_phone"]);
+    }
+    //name
+    if (empty($_POST["contact_name"])) {
+        $error = "Name is required";
+    } else {
+        $name = validate_input($_POST["contact_name"]);
+    }
+
+
+    //send email
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->SMTPDebug = 2;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.dreamhost.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $mail_user;
+        $mail->Password = $mail_pass;
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->setFrom('zack@zackmdesigns.com', 'WEBSITE');
+        $mail->addAddress('zackmdesigns@gmail.com', 'Zack');
+
+        if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+            $mail->Subject = 'PHPMailer contact form';
+            //Keep it simple - don't use HTML
+            $mail->isHTML(false);
+            //Build a simple message body
+            $mail->Body = <<<EOT
+                Email: {$email}
+                Name: {$name}
+                Phone: {$phone}
+                EOT;
+
+            if (!$mail->send()) {
+                //The reason for failing to send will be in $mail->ErrorInfo
+                //but you shouldn't display errors to users - process the error, log it on your server.
+                $msg = 'Sorry, something went wrong. Please try again later.';
+            } 
+            else {
+                $msg = 'Message sent!';
+            }
+        }
+        else {
+            $msg = 'Invalid email address.';
+        }
+    }
+}
+
+function validate_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
